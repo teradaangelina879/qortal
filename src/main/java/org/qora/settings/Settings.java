939ce0d652585c65a35cfac56aa319765efb0a23
catbref <misc-github@talk2dom.com>
2019-11-26 10:26:16 +0000

    Correct auto-update URLs to use "qortal.update" not "qortal.jar"

--- src/main/java/org/qora/settings/Settings.java
+++ src/main/java/org/qora/settings/Settings.java
@@ -95,8 +95,8 @@ public class Settings {
 
 	// Auto-update sources
 	private String[] autoUpdateRepos = new String[] {
-		"https://github.com/QORT/qortal/raw/%s/qortal.jar",
-		"https://raw.githubusercontent.com@151.101.16.133/QORT/qortal/%s/qortal.jar"
+		"https://github.com/QORT/qortal/raw/%s/qortal.update",
+		"https://raw.githubusercontent.com@151.101.16.133/QORT/qortal/%s/qortal.update"
 	};
 
 	/** Array of NTP server hostnames. */
