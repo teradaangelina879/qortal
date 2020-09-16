af7d7d096622ae97220b44eda8712cadfc2acc8d
catbref <misc-github@talk2dom.com>
2020-09-16 10:57:45 +0100

    Unified Bitcoin/Litecoin test apps

--- src/test/java/org/qortal/test/crosschain/bitcoinv1/Common.java
+++
@@ -1,9 +0,0 @@
-package org.qortal.test.crosschain.bitcoinv1;
-
-import org.bitcoinj.core.Coin;
-
-public abstract class Common {
-
-	public static final Coin DEFAULT_BTC_FEE = Coin.parseCoin("0.00001000");
-
-}
