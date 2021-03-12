teradaangelina879/qortal

commit 33a8f311e5b8d9ae3d62420f0bac365bcfe4e113

Reduce logging noise from lost trade-bot ATs and self-clean if AT does not exist after 24 hours

src/main/java/org/qortal/controller/tradebot/BitcoinACCTv1TradeBot.java
@@ -382,7 +382,16 @@ public class BitcoinACCTv1TradeBot implements AcctTradeBot {
 			// Attempt to fetch AT data
 			atData = repository.getATRepository().fromATAddress(tradeBotData.getAtAddress());
 			if (atData == null) {
-				LOGGER.warn(() -> String.format("Unable to fetch trade AT %s from repository", tradeBotData.getAtAddress()));
+				LOGGER.debug(() -> String.format("Unable to fetch trade AT %s from repository", tradeBotData.getAtAddress()));
+
+				// If it has been over 24 hours since we last updated this trade-bot entry then assume AT is never coming back
+				// and so wipe the trade-bot entry
+				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD > NTP.getTime()) {
+					LOGGER.info(() -> String.format("AT %s has been gone for too long - deleting trade-bot entry", tradeBotData.getAtAddress()));
+					repository.getCrossChainRepository().delete(tradeBotData.getTradePrivateKey());
+					repository.saveChanges();
+				}
+
 				return;
 			}
 

src/main/java/org/qortal/controller/tradebot/LitecoinACCTv1TradeBot.java
@@ -380,7 +380,16 @@ public class LitecoinACCTv1TradeBot implements AcctTradeBot {
 			// Attempt to fetch AT data
 			atData = repository.getATRepository().fromATAddress(tradeBotData.getAtAddress());
 			if (atData == null) {
-				LOGGER.warn(() -> String.format("Unable to fetch trade AT %s from repository", tradeBotData.getAtAddress()));
+				LOGGER.debug(() -> String.format("Unable to fetch trade AT %s from repository", tradeBotData.getAtAddress()));
+
+				// If it has been over 24 hours since we last updated this trade-bot entry then assume AT is never coming back
+				// and so wipe the trade-bot entry
+				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD > NTP.getTime()) {
+					LOGGER.info(() -> String.format("AT %s has been gone for too long - deleting trade-bot entry", tradeBotData.getAtAddress()));
+					repository.getCrossChainRepository().delete(tradeBotData.getTradePrivateKey());
+					repository.saveChanges();
+				}
+
 				return;
 			}
 

commit 5fc495eb6a633ac4a503c0f9aabbc08d62ec9635

Fix for possible logic bug introduced in commit 33a8f31.

src/main/java/org/qortal/controller/tradebot/BitcoinACCTv1TradeBot.java
@@ -386,7 +386,7 @@ public class BitcoinACCTv1TradeBot implements AcctTradeBot {
 
 				// If it has been over 24 hours since we last updated this trade-bot entry then assume AT is never coming back
 				// and so wipe the trade-bot entry
-				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD > NTP.getTime()) {
+				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD < NTP.getTime()) {
 					LOGGER.info(() -> String.format("AT %s has been gone for too long - deleting trade-bot entry", tradeBotData.getAtAddress()));
 					repository.getCrossChainRepository().delete(tradeBotData.getTradePrivateKey());
 					repository.saveChanges();

src/main/java/org/qortal/controller/tradebot/LitecoinACCTv1TradeBot.java
@@ -384,7 +384,7 @@ public class LitecoinACCTv1TradeBot implements AcctTradeBot {
 
 				// If it has been over 24 hours since we last updated this trade-bot entry then assume AT is never coming back
 				// and so wipe the trade-bot entry
-				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD > NTP.getTime()) {
+				if (tradeBotData.getTimestamp() + MAX_AT_CONFIRMATION_PERIOD < NTP.getTime()) {
 					LOGGER.info(() -> String.format("AT %s has been gone for too long - deleting trade-bot entry", tradeBotData.getAtAddress()));
 					repository.getCrossChainRepository().delete(tradeBotData.getTradePrivateKey());
 					repository.saveChanges();