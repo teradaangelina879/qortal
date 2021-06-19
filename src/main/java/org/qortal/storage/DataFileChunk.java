f296ec46c8033733887a20f80943ca7a00f9c3f5
CalDescent <caldescent@protonmail.com>
2021-06-19 17:32:55 +0100

    Removed unused headers.

--- src/main/java/org/qortal/storage/DataFileChunk.java
+++ src/main/java/org/qortal/storage/DataFileChunk.java
@@ -2,11 +2,7 @@ package org.qortal.storage;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.qortal.crypto.Crypto;
-import org.qortal.utils.Base58;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
