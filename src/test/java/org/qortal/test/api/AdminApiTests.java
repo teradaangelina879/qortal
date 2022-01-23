ff6ec83b1cc94e81e6212d2ec64bc262f2ad97fc
CalDescent <caldescent@protonmail.com>
2022-01-23 12:48:37 +0000

    Removed localAuthBypassEnabled override in unit tests.
    Hopefully this will allow us to proactively catch any missing API keys in the future.

--- src/test/java/org/qortal/test/api/AdminApiTests.java
+++ src/test/java/org/qortal/test/api/AdminApiTests.java
@@ -2,10 +2,12 @@ package org.qortal.test.api;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.qortal.api.resource.AdminResource;
 import org.qortal.repository.DataException;
+import org.qortal.settings.Settings;
 import org.qortal.test.common.ApiCommon;
 import org.qortal.test.common.Common;
 
@@ -29,7 +31,10 @@ public class AdminApiTests extends ApiCommon {
 	}
 
 	@Test
-	public void testSummary() {
+	public void testSummary() throws IllegalAccessException {
+		// Set localAuthBypassEnabled to true, since we don't need to test authentication here
+		FieldUtils.writeField(Settings.getInstance(), "localAuthBypassEnabled", true, true);
+
 		assertNotNull(this.adminResource.summary("testApiKey"));
 	}
 
