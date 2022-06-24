db8f627f1a76e1dd2bc0f18496e990f205d4b86a
CalDescent <caldescent@protonmail.com>
2022-06-24 10:13:55 +0100

    Default minPeerVersion set to 3.3.7

--- src/main/java/org/qortal/settings/Settings.java
+++ src/main/java/org/qortal/settings/Settings.java
@@ -203,7 +203,7 @@ public class Settings {
 	private int maxRetries = 2;
 
 	/** Minimum peer version number required in order to sync with them */
-	private String minPeerVersion = "3.1.0";
+	private String minPeerVersion = "3.3.7";
 	/** Whether to allow connections with peers below minPeerVersion
 	 * If true, we won't sync with them but they can still sync with us, and will show in the peers list
 	 * If false, sync will be blocked both ways, and they will not appear in the peers list */
