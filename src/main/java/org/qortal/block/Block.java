6003ed3ff7fc51762f9627ce421fdae9f48b762e
CalDescent <caldescent@protonmail.com>
2022-09-10 17:23:39 +0100

    Revert "Use block's online accounts timestamp (instead of main timestamp) for the mempow hard fork."
    This reverts commit 8cca6db31637dcf43cbcf524949f629c3b35b6a6.

--- src/main/java/org/qortal/block/Block.java
+++ src/main/java/org/qortal/block/Block.java
@@ -407,7 +407,7 @@ public class Block {
 		byte[] onlineAccountsSignatures = Qortal25519Extras.aggregateSignatures(signaturesToAggregate);
 
 		// Add nonces to the end of the online accounts signatures if mempow is active
-		if (onlineAccountsTimestamp >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
+		if (timestamp >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
 			try {
 				// Create ordered list of nonce values
 				List<Integer> nonces = new ArrayList<>();
@@ -1041,7 +1041,7 @@ public class Block {
 		final int signaturesLength = Transformer.SIGNATURE_LENGTH;
 		final int noncesLength = onlineRewardShares.size() * Transformer.INT_LENGTH;
 
-		if (this.blockData.getOnlineAccountsTimestamp() >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
+		if (this.blockData.getTimestamp() >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
 			// We expect nonces to be appended to the online accounts signatures
 			if (this.blockData.getOnlineAccountsSignatures().length != signaturesLength + noncesLength)
 				return ValidationResult.ONLINE_ACCOUNT_SIGNATURES_MALFORMED;
@@ -1057,7 +1057,7 @@ public class Block {
 		byte[] encodedOnlineAccountSignatures = this.blockData.getOnlineAccountsSignatures();
 
 		// Split online account signatures into signature(s) + nonces, then validate the nonces
-		if (this.blockData.getOnlineAccountsTimestamp() >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
+		if (this.blockData.getTimestamp() >= BlockChain.getInstance().getOnlineAccountsMemoryPoWTimestamp()) {
 			byte[] extractedSignatures = BlockTransformer.extract(encodedOnlineAccountSignatures, 0, signaturesLength);
 			byte[] extractedNonces = BlockTransformer.extract(encodedOnlineAccountSignatures, signaturesLength, onlineRewardShares.size() * Transformer.INT_LENGTH);
 			encodedOnlineAccountSignatures = extractedSignatures;
