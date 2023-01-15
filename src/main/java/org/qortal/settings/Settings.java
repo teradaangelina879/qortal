dfe3754afc3d3ede3e7f4722a1baae9f4432c324
CalDescent <caldescent@protonmail.com>
2023-01-15 12:07:27 +0000

    Block connections with peers older than 3.8.2, as those versions are nonfunctional due to recent feature triggers.

--- src/main/java/org/qortal/settings/Settings.java
+++ src/main/java/org/qortal/settings/Settings.java
@@ -215,7 +215,7 @@ public class Settings {
 	public long recoveryModeTimeout = 10 * 60 * 1000L;
 
 	/** Minimum peer version number required in order to sync with them */
-	private String minPeerVersion = "3.8.0";
+	private String minPeerVersion = "3.8.2";
 	/** Whether to allow connections with peers below minPeerVersion
 	 * If true, we won't sync with them but they can still sync with us, and will show in the peers list
 	 * If false, sync will be blocked both ways, and they will not appear in the peers list */
